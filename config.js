1f5a3c21eca6da38cb82c843249575d8f18e33ac
Keyframe <58492865+ArchitecturalVisualisation@users.noreply.github.com>
2020-07-07 12:28:08 +0200

    immovis
    360

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Tuesday 7th of July 2020 10:24:03 AM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost',//path to the index.html
+    theme : 'theasys',
+};
